ccd30b7e4c343e13415257ba9ba6b56bf64a1bf5
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-02-26 10:18:14 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Impiccato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] facili = {"cane", "casa", "amico", "pane", "orizzonte", "biscotto"};
+            string[] medie = { "aiuola", "corteo", "tanfo", "acquitrino", "vorace", "canaglia" };
+            string[] difficili = { "Hippopotomonstrosesquipedaliofobia", "supercalifragilistichespiralitoso", "obbiettivo", "entusiasmo", "contesto", "priorita" };
+
+            scelta
+        }
+    }
+}
