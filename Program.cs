1dbab271081311aae9806abb8239dd1066623812
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-03-04 13:21:22 +0100

    bene

--- Program.cs
+++ Program.cs
@@ -2,61 +2,91 @@
 {
     internal class Program
     {
-        static void facile(string[] facili, ref int vita, char[] lettere)
+        static string parolaNascosta(string parola) // funzione che restituisce una stringa con tanti "_" quanti sono i caratteri della parola da indovinare
         {
-            Random random = new Random();
-            int num = random.Next(0, facili.Length);
             string risultato = "";
-            bool trovato = false;
-            int a = 0;
-
-            for (int i = 0; i < facili[num].Length; i++)
+            for (int i = 0; i < parola.Length; i++)
             {
                 risultato += "_";
             }
+            return risultato;
+        }
+        static void parola(string[] facili, ref int vita, char[] lettere)
+        {
+            Random random = new Random();
+            int num = random.Next(0, facili.Length);
 
-            Console.WriteLine(risultato);
-            char[] risultatoArray = risultato.ToCharArray();
+            bool trovato = false, fine = false;
+            int a = 0;
+
+            Console.WriteLine(parolaNascosta(facili[num]));
+            char[] risultatoArray = parolaNascosta(facili[num]).ToCharArray();
 
             facili[num] = facili[num].ToLower();
 
-            while (vita > 0)
+            while (vita > 0 && fine == false) // ciclo che continua finché il giocatore ha vite e non ha indovinato la parola
             {
-                Console.WriteLine("Inserisci una lettera: ");
-                char lettera = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Vuoi indovinare la parola? (s/n)");
+                char risposta = Convert.ToChar(Console.ReadLine());
 
-                trovato = facili[num].Contains(lettera);
-
-                if(trovato == true)
+                if (risposta == 's')
                 {
-                    
-                    for(int i = 0; i < facili[num].Length; i++)
+                    Console.WriteLine("Inserisci la parola: ");
+                    string parola = Convert.ToString(Console.ReadLine());
+
+                    if (parola == facili[num])
                     {
-                        if (facili[num][i] == lettera)
-                        {
-                            risultatoArray[i] = lettera;
-                        }
+                        Console.WriteLine("Hai vinto!");
+                        fine = true;
                     }
-                    Console.WriteLine(risultatoArray);
-                    
-                    lettere[a] = lettera;
-                    a++;
+                    else
+                    {
+                        Console.WriteLine("Parola sbagliata, ti rimangono " + vita + " vite");
+                        vita--;
+                    }
                 }
                 else
                 {
-                    vita--;
-                    Console.WriteLine("Lettera non trovata, ti rimangono " + vita + " vite");
+                    Console.WriteLine("Inserisci una lettera: ");
+                    char lettera = Convert.ToChar(Console.ReadLine());
 
-                    lettere[a] = lettera;
-                    a++;
+                    trovato = facili[num].Contains(lettera);
+
+                    if (trovato == true)
+                    {
+                        for (int i = 0; i < facili[num].Length; i++) // ciclo che scorre la parola da indovinare
+                        {
+                            if (facili[num][i] == lettera) // se la lettera è presente nella parola, viene sostituita al posto del "_" corrispondente
+                            {
+                                risultatoArray[i] = lettera;
+                            }
+                        }
+                        Console.WriteLine(risultatoArray);
+
+                        lettere[a] = lettera;
+                        a++;
+                    }
+                    else
+                    {
+                        vita--;
+                        Console.WriteLine("Lettera non trovata, ti rimangono " + vita + " vite");
+
+                        lettere[a] = lettera;
+                        a++;
+                    }
                 }
-
+                
                 Console.Write("Lettere inserite: ");
                 for(int i = 0; i < lettere.Length; i++)
                 {
                     Console.Write(lettere[i] + " ");
                 }
                 Console.WriteLine();
+            }
+
+            if(vita == 0)
+            {
+                Console.WriteLine("Hai perso! La parola era: " + facili[num]);
             }
         }
         static void Main(string[] args)
@@ -74,51 +104,102 @@
             string[] sportDifficili = { "Pallamano", "Hockey su ghiaccio", "Curling", "Scherma", "Equitazione", "Tiro con arco", "Lotta" };
 
             char[] lettere = new char[30];
-            int vita = 116, scelta;
+            int vita = 10, scelta;
+            bool rigiocare = false;
 
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("| Scegli la categoria        |");
-            Console.WriteLine("| 1) Cibi                    |");
-            Console.WriteLine("| 2) Animali                 |");
-            Console.WriteLine("| 3) Sport                   |");
-            Console.WriteLine("-----------------------------");
-            int categoria = Convert.ToInt32(Console.ReadLine());
-
-            if (categoria == 1)
+            while (rigiocare == false)
             {
                 Console.WriteLine("-----------------------------");
-                Console.WriteLine("| Scegli la modalià         |");
-                Console.WriteLine("| 1) facile                 |");
-                Console.WriteLine("| 2) media                  |");
-                Console.WriteLine("| 3) difficile              |");
+                Console.WriteLine("| Scegli la categoria        |");
+                Console.WriteLine("| 1) Cibi                    |");
+                Console.WriteLine("| 2) Animali                 |");
+                Console.WriteLine("| 3) Sport                   |");
                 Console.WriteLine("-----------------------------");
-                scelta = Convert.ToInt32(Console.ReadLine());
+                int categoria = Convert.ToInt32(Console.ReadLine());
 
-                if(scelta == 1)
+                if (categoria == 1)
                 {
-                    facile(cibiFacile, ref vita, lettere);
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("| Scegli la modalià         |");
+                    Console.WriteLine("| 1) facile                 |");
+                    Console.WriteLine("| 2) media                  |");
+                    Console.WriteLine("| 3) difficile              |");
+                    Console.WriteLine("-----------------------------");
+                    scelta = Convert.ToInt32(Console.ReadLine());
+
+                    if (scelta == 1)
+                    {
+                        parola(cibiFacile, ref vita, lettere);
+                    }
+                    else if (scelta == 2)
+                    {
+                        parola(cibiMedio, ref vita, lettere);
+                    }
+                    else if (scelta == 3)
+                    {
+                        parola(cibiDifficili, ref vita, lettere);
+                    }
                 }
-                
-            }
-            else if (categoria == 2)
-            {
-                Console.WriteLine("-----------------------------");
-                Console.WriteLine("| Scegli la modalià         |");
-                Console.WriteLine("| 1) facile                 |");
-                Console.WriteLine("| 2) media                  |");
-                Console.WriteLine("| 3) difficile              |");
-                Console.WriteLine("-----------------------------");
-                scelta = Convert.ToInt32(Console.ReadLine());
-            }
-            else if (categoria == 3)
-            {
-                Console.WriteLine("-----------------------------");
-                Console.WriteLine("| Scegli la modalià         |");
-                Console.WriteLine("| 1) facile                 |");
-                Console.WriteLine("| 2) media                  |");
-                Console.WriteLine("| 3) difficile              |");
-                Console.WriteLine("-----------------------------");
-                scelta = Convert.ToInt32(Console.ReadLine());
+                else if (categoria == 2)
+                {
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("| Scegli la modalià         |");
+                    Console.WriteLine("| 1) facile                 |");
+                    Console.WriteLine("| 2) media                  |");
+                    Console.WriteLine("| 3) difficile              |");
+                    Console.WriteLine("-----------------------------");
+                    scelta = Convert.ToInt32(Console.ReadLine());
+
+                    if (scelta == 1)
+                    {
+                        parola(animaliFacile, ref vita, lettere);
+                    }
+                    else if (scelta == 2)
+                    {
+                        parola(animaliMedio, ref vita, lettere);
+                    }
+                    else if (scelta == 3)
+                    {
+                        parola(animaliDifficili, ref vita, lettere);
+                    }
+                }
+                else if (categoria == 3)
+                {
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("| Scegli la modalià         |");
+                    Console.WriteLine("| 1) facile                 |");
+                    Console.WriteLine("| 2) media                  |");
+                    Console.WriteLine("| 3) difficile              |");
+                    Console.WriteLine("-----------------------------");
+                    scelta = Convert.ToInt32(Console.ReadLine());
+
+                    if (scelta == 1)
+                    {
+                        parola(sportFacile, ref vita, lettere);
+                    }
+                    else if (scelta == 2)
+                    {
+                        parola(sportMedio, ref vita, lettere);
+                    }
+                    else if (scelta == 3)
+                    {
+                        parola(sportDifficili, ref vita, lettere);
+                    }
+                }
+
+                Console.WriteLine("Vuoi rigiocare? (s/n)");
+                string risposta = Convert.ToString(Console.ReadLine());
+
+                if (risposta == "s")
+                {
+                    rigiocare = false;
+                    vita = 10;
+                    lettere = new char[30];
+                }
+                else
+                {
+                    rigiocare = true;
+                }
             }
         }
     }
